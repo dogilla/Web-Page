bea2f5ddb714be0ad8035039aba3e7b63f98a315
dogilla <19thmagm@ciencias.unam.mx>
2019-07-05 19:23:08 -0500

    orientacion a objetos

--- script1.js
+++ script1.js
@@ -1,3 +1,31 @@
 function apretar(){
-    alert("Hola que hace");
+    alert("hola 1");
 }; 
+
+class circulo {
+    constructor(area, perimetro){
+        this.area = area;
+        this.perimetro = perimetro;
+    }
+
+    setPerimetro(per) {
+        this.perimetro = per
+    }
+}
+
+
+//creando un evento
+var eventoUno = document.querySelector(".tres"); 
+eventoUno.addEventListener("click", function(){
+    var c = new circulo(2,3)
+    document.write(c.perimetro + "<br>");
+    c.setPerimetro(6);
+    document.write(c.perimetro);
+});
+
+var a = document.querySelector("#dos");
+a.style.color = "red"; 
+
+
+
+
