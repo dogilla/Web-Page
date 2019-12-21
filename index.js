1adfacffa04b7ef1292d87e7015dfb305597fca1
dogilla <19thmagm@ciencias.unam.mx>
2019-12-21 14:08:59 -0600

    configuraciones

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+//set static folder
+app.use(express.static(path.join(__dirname, 'static'))); 
 
-app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
-});
 
-app.listen(3000, () => console.log('Escuchando por puerto 3000'));
+app.listen(3000, () => {
+    console.log('Escuchando por puerto 3000');
+});
